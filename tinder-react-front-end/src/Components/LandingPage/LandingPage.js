91e4b1f507bb9af13d42814245f8e15c5c614501
jthompkins <jared.thompkins@outlook.com>
2024-06-23 00:48:12 -0700

    add sign up ui

--- tinder-react-front-end/src/Components/LandingPage/LandingPage.js
+++ tinder-react-front-end/src/Components/LandingPage/LandingPage.js
@@ -1,8 +1,20 @@
-import React from 'react';
+// src/Components/LandingPage/LandingPage.js
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './LandingPage.css';
+import SignUpForm from '../SignUpForm/SignUpForm';
 
 const LandingPage = () => {
+    const [showSignUp, setShowSignUp] = useState(false);
+
+    const handleCreateAccountClick = () => {
+        setShowSignUp(true);
+    };
+
+    const handleClose = () => {
+        setShowSignUp(false);
+    };
+
     return (
         <div className="landing-page">
             <h1><span className="bold">It Starts</span> <span className="unbold">with a</span> <span className="bold">Swipe</span></h1>
@@ -11,16 +23,18 @@ const LandingPage = () => {
                     By tapping ‘Create account’ or ‘Sign in’ you agree to our Terms. Learn how we process your data in our Privacy Policy and Cookies Policy.
                 </p>
                 <div className="buttons">
-                    <Link to="/home" className="button create-account">Create account</Link>
+                    <button onClick={handleCreateAccountClick} className="button create-account">Create account</button>
                     <Link to="/home" className="button sign-in">Sign In</Link>
                     <Link to="/home" className="trouble-signing-in">Trouble signing in?</Link>
                 </div>
             </div>
+            {showSignUp && <SignUpForm onClose={handleClose} />}
         </div>
     );
 }
 
 export default LandingPage;
+
 
 
 
