f014e37c584425bb6e8a8e95d1094f4b64e45c17
Ethan  Feiges <ejfeiges@hotmail.com>
2024-06-23 00:30:09 -0700

    Integrated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ProfileCard from './ProfileCard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './Components/LandingPage/LandingPage';
+import Header from './Components/Header/Header';
+import SwipeButtons from './Components/SwipeButtons/SwipeButtons';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <ProfileCard />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={
+            <>
+              <Header />
+              <ProfileCard />
+              <SwipeButtons />
+            </>
+          } />
+          {/* Add other routes here if needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
