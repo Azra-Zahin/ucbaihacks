63c5120dde71f291d66c6a1bdae7308a8d4c05b0
jthompkins <jared.thompkins@outlook.com>
2024-06-22 20:24:27 -0700

    add matching ui

---
+++ tinder-react-font-end/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+// import components
+import Header from "./Components/Header/Header";
+import SwipeButtons from "./Components/SwipeButtons/SwipeButtons";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <SwipeButtons />
+
+    </div>
+  );
+}
+
+export default App;
