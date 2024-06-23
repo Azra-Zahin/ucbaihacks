2bf5bc01c9a17bd4c4f3a0eb1552a7c387031659
Azra Zahin <azraz@uci.edu>
2024-06-22 17:29:14 -0700

    tinder inspired template

--- hack-match/src/App.js
+++ hack-match/src/App.js
@@ -1,10 +1,17 @@
-
-
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Profile from './pages/Profile'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
-    <div className="App">
-    </div>
-  );
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
